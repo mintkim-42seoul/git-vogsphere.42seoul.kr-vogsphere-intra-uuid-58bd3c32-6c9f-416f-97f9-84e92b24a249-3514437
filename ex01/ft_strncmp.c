24a8088a36b522491e18b5e8e145e171814ffb5c
mintkim-42seoul <doublint@naver.com>
2021-04-07 18:41:34 +0900

    modified

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mintkim <mintkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 20:58:53 by mintkim           #+#    #+#             */
-/*   Updated: 2021/04/07 12:27:13 by mintkim          ###   ########.fr       */
+/*   Updated: 2021/04/07 18:40:19 by mintkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 		{
@@ -23,5 +23,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		}
 		i++;
 	}
-	return (0);
+	if (n == 0)
+		return (0);
+	return (s1[i] - s2[i]);
 }
