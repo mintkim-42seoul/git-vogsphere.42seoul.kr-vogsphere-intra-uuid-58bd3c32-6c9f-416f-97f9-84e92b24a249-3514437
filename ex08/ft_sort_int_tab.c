4ecbb0102a7b559ccfb8b012a5b62fa0f081692f
mintkim-42seoul <doublint@naver.com>
2021-04-08 22:42:03 +0900

    modified

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,34 +6,28 @@
 /*   By: mintkim <mintkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 16:36:11 by mintkim           #+#    #+#             */
-/*   Updated: 2021/04/03 19:00:50 by mintkim          ###   ########.fr       */
+/*   Updated: 2021/04/08 22:41:22 by mintkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int i;
 	int j;
-	int cnt;
 	int sw;
 
 	i = 0;
-	cnt = size - 1;
-	while (i < size)
+	while (i++ < size)
 	{
 		j = 0;
-		while (j < cnt)
+		while (++j < size)
 		{
-			if (tab[j] > tab[j + 1])
+			if (tab[j - 1] > tab[j])
 			{
-				sw = tab[j];
-				tab[j] = tab[j + 1];
-				tab[j + 1] = sw;
+				sw = tab[j - 1];
+				tab[j - 1] = tab[j];
+				tab[j] = sw;
 			}
-			++j;
 		}
-		--cnt;
 	}
 }
