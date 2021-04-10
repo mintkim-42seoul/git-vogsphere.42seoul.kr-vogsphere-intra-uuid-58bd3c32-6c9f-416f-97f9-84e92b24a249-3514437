667a2416238e5cbbb3614f959290361f7c74058f
mintkim-42seoul <doublint@naver.com>
2021-04-10 21:14:38 +0900

    modi

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,20 +6,20 @@
 /*   By: mintkim <mintkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 12:21:44 by mintkim           #+#    #+#             */
-/*   Updated: 2021/04/10 16:28:08 by mintkim          ###   ########.fr       */
+/*   Updated: 2021/04/10 21:12:22 by mintkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
 	char c;
 	char d;
 
-	if(nb == -2147483648)
+	if (nb == -2147483648)
 	{
-		write (1, "-2147483648", 11);
+		write(1, "-2147483648", 11);
 	}
 	else
 	{
@@ -35,7 +35,7 @@ void ft_putnbr(int nb)
 			if (nb > 9)
 				ft_putnbr(nb / 10);
 			c = 48 + nb % 10;
-			write (1, &c, 1);
+			write(1, &c, 1);
 		}
 	}
 }
