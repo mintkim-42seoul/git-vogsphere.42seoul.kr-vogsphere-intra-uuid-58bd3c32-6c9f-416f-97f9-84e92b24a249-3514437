5b3f5a2949b4fc1b45ff288808fe7b8ea6d13d90
mintkim-42seoul <doublint@naver.com>
2021-04-13 00:10:34 +0900

    modi

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,34 +6,18 @@
 /*   By: mintkim <mintkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 15:45:05 by mintkim           #+#    #+#             */
-/*   Updated: 2021/04/10 17:46:00 by mintkim          ###   ########.fr       */
+/*   Updated: 2021/04/12 23:36:12 by mintkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	recc(char *base, int nbr, int radix)
+void	recc(char *base, long long nbr, int radix)
 {
-	int origin_radix;
-	int cnt;
-	int i;
-
-	origin_radix = radix;
-	cnt = 0;
-	while (nbr / radix >= origin_radix)
-	{
-		radix = radix * origin_radix;
-		cnt++;
-	}
-	i = cnt;
-	while (i >= 0)
-	{
-		write(1, &base[nbr / radix], 1);
-		nbr = nbr % radix;
-		radix = radix / origin_radix;
-		i--;
-	}
-	write(1, &base[nbr % origin_radix], 1);
+	if (nbr == 0)
+		return ;
+	recc(base, nbr / radix, radix);
+	write(1, &base[nbr % radix], 1);
 }
 
 int		count(char *base)
@@ -42,9 +26,7 @@ int		count(char *base)
 
 	i = 0;
 	while (base[i])
-	{
 		i++;
-	}
 	return (i);
 }
 
@@ -57,16 +39,14 @@ int		ccheck(char *base)
 	while (base[i])
 	{
 		j = 0;
-		if (base[i] == '+' || base[i] == '-')
-		{
+		if (base[i] == '+' || base[i] == '-' || base[i] == '\t'
+				|| base[i] == '\n' || base[i] == '\v' || base[i] == '\f'
+				|| base[i] == '\r' || base[i] == ' ')
 			return (0);
-		}
 		while (base[j])
 		{
 			if (i != j && base[i] == base[j])
-			{
 				return (0);
-			}
 			j++;
 		}
 		i++;
@@ -78,28 +58,20 @@ int		ccheck(char *base)
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int		radix;
-	int		minus;
-	char	c;
+	int				radix;
+	char			c;
+	long long		good;
 
 	radix = count(base);
+	good = nbr;
 	if (ccheck(base) == 1)
 	{
-		c = '-';
-		if (nbr < 0)
+		if (good < 0)
 		{
+			c = '-';
 			write(1, &c, 1);
-			if (nbr == -2147483648)
-			{
-				minus = nbr / radix;
-				minus = -minus;
-				recc(base, minus, radix);
-				write(1, &base[-(nbr % radix)], 1);
-				return ;
-			}
-			else
-				nbr = -nbr;
+			good = -good;
 		}
-		recc(base, nbr, radix);
+		recc(base, good, radix);
 	}
 }
--- ex05/ft_atoi_base.c
+++ ex05/ft_atoi_base.c
@@ -6,45 +6,14 @@
 /*   By: mintkim <mintkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 17:44:22 by mintkim           #+#    #+#             */
-/*   Updated: 2021/04/10 17:45:50 by mintkim          ###   ########.fr       */
+/*   Updated: 2021/04/13 00:10:08 by mintkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 char	g_digit[100000];
-int		g_cq = 0;
-int		g_len = 0;
-int		g_sign = 1;
-int		g_flag = 1;
 int		g_cnt = 0;
-
-int		rec(char *base, int radix)
-{
-	int dbl;
-	int i;
-	int j;
-
-	i = 0;
-	while (g_digit[g_len])
-		g_len++;
-	while (i < g_len)
-	{
-		j = 0;
-		dbl = 1;
-		while (i - j > 0)
-		{
-			dbl = dbl * radix;
-			j++;
-		}
-		j = 0;
-		while (base[j] != g_digit[g_len - i - 1] && base[j])
-			j++;
-		if (j == radix)
-			return (0);
-		g_cq = dbl * j + g_cq;
-		i++;
-	}
-	return (g_cq);
-}
 
 int		check(char *base)
 {
@@ -55,17 +24,19 @@ int		check(char *base)
 	while (base[i])
 	{
 		j = 0;
+		if (base[i] == '+' || base[i] == '-' || base[i] == '\t'
+				|| base[i] == '\n' || base[i] == '\v' || base[i] == '\f'
+				|| base[i] == '\r' || base[i] == ' ')
+			return (0);
 		while (base[j])
 		{
 			if (i != j && base[i] == base[j])
 				return (0);
 			j++;
 		}
-		if (base[i] == '+' || base[i] == '-' || base[i] == ' ')
-			return (0);
 		i++;
 	}
-	if (i == 1 || i == 0)
+	if (i == 0 || i == 1)
 		return (0);
 	return (1);
 }
@@ -96,31 +67,58 @@ int		ft_atoii(char *str)
 	return (sign);
 }
 
+int		base_check(char *base, char c)
+{
+	int i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+void	init(void)
+{
+	int i;
+
+	i = 0;
+	while (g_digit[i])
+	{
+		g_digit[i] = 0;
+		i++;
+	}
+	g_cnt = 0;
+}
+
 int		ft_atoi_base(char *str, char *base)
 {
 	int sign;
 	int i;
-	int ret;
+	int radix;
+	int retur;
 
 	i = 0;
-	while (base[i])
-		i++;
-	sign = ft_atoii(str);
+	retur = 0;
+	radix = 0;
+	init();
+	while (base[radix])
+		radix++;
 	if (check(base) == 0)
 		return (0);
-	ret = rec(base, i);
-	if (sign == -1)
-		ret = -ret;
-	i = 0;
-	g_cnt = 0;
-	g_cq = 0;
-	g_len = 0;
+	sign = ft_atoii(str);
 	while (g_digit[i])
 	{
-		g_digit[i] = '\0';
+		if (base_check(base, g_digit[i]) == -1)
+			return (0);
+		retur = retur * radix;
+		retur = retur + base_check(base, g_digit[i]);
 		i++;
 	}
-	g_flag = 1;
-	g_sign = 1;
-	return (ret);
+	if (sign == -1)
+		retur = -retur;
+	return (retur);
 }
