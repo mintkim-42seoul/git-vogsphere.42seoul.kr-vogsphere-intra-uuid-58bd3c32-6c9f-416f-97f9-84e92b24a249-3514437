30e48d693fbeb4fa81e7293321f515098a30e8b7
mintkim-42seoul <doublint@naver.com>
2021-07-30 14:55:58 +0900

    modi

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mintkim <mintkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 20:05:38 by mintkim           #+#    #+#             */
-/*   Updated: 2021/07/30 14:50:30 by mintkim          ###   ########.fr       */
+/*   Updated: 2021/07/30 14:55:37 by mintkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	leng;
 	char	*ret;
 
-	if (!s)
-		return (0);
 	ret = (char *)malloc(sizeof(char) * (len + 1));
 	if (ret == 0)
 		return (0);
-	leng = ft_strlen((char *)s);
 	i = 0;
 	j = 0;
 	while (s[i] != '\0')
 	{
-		if (i >= start && j <= leng)
+		if (i >= start && j <= len)
 			ret[j++] = s[i];
 		i++;
 	}
